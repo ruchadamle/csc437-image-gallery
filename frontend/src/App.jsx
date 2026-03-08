8684955b76bc21eb10d1fe31d0b22434f875e9d9
ruchadamle <ruchadamle@gmail.com>
2026-03-07 16:31:24 -0800

    Completed Lab 19

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -4,15 +4,16 @@ import { UploadPage } from "./UploadPage.jsx";
 import { LoginPage } from "./LoginPage.jsx";
 import { MainLayout } from "./MainLayout.jsx";
 import { Route, Routes } from "react-router";
+import { VALID_ROUTES } from "../../shared/ValidRoutes.js";
 
 function App() {
     return (
         <Routes>
-            <Route path="/" element={<MainLayout />}>
-                <Route index element={<AllImages />} />
-                <Route path="images/:imageId" element={<ImageDetails />} />
-                <Route path="upload" element={<UploadPage />} />
-                <Route path="login" element={<LoginPage />} />
+            <Route element={<MainLayout />}>
+                <Route path={VALID_ROUTES.HOME} element={<AllImages />} />
+                <Route path={VALID_ROUTES.IMAGE_DETAILS} element={<ImageDetails />} />
+                <Route path={VALID_ROUTES.UPLOAD} element={<UploadPage />} />
+                <Route path={VALID_ROUTES.LOGIN} element={<LoginPage />} />
             </Route>
         </Routes>
     );
