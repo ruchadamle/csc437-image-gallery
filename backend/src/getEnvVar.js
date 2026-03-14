8aee708394e48426d18951ed2ed8e14c53ee2113
ruchadamle <ruchadamle@gmail.com>
2026-03-13 19:10:15 -0700

    Completed lab 23a

--- backend/src/getEnvVar.js
+++ backend/src/getEnvVar.js
@@ -1,15 +1,7 @@
 import dotenv from "dotenv";
 
-dotenv.config(); // Read the .env file in the current working directory, and load values into process.env.
-// Even if this file is imported in multiple places, this will run only once.
+dotenv.config();
 
-/**
- * Gets the value of the environment variable with the specified name, or `undefined` if it isn't set.
- *
- * @param varName - the name of the environment variable to read
- * @param warnIfNotSet - whether to print a warning to the console if the variable isn't set (default true)
- * @returns {string} the value of the environment variable, or `undefined` if it isn't set
- */
 export function getEnvVar(varName, warnIfNotSet=true) {
     const value = process.env[varName];
     if (warnIfNotSet && !value) {
